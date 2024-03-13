6f16940d062e00f78e202e91e64e1e1c0bc7391d
MetMet7 <159765915+MetMet7@users.noreply.github.com>
2024-03-13 10:29:42 +0100

    Update data_visualiser.py

--- SPAC Uge 6/data_visualiser.py
+++ SPAC Uge 6/data_visualiser.py
@@ -1,56 +1,64 @@
-from data_analyser import *
-import pandas as pd
+from data_analyser import Analyser
 import matplotlib.pyplot as plt
+import pandas as pd
 
-def sales_over_time_plot(df):
-    sales_per_day = sales_over_time(df)
-    fig, axs = plt.subplots()
-    sales_per_day.plot(ax = axs)
-    axs.set_xlabel('Date')
-    axs.set_ylabel('Income')
-    fig.autofmt_xdate()
-    fig.savefig('Sales over Time.png')
-    return fig
+class Plots:
 
-def cumulative_sales_plot(df):
-    cumulative = cumulative_sales(df)
-    fig, axs = plt.subplots(figsize= (40,4))
-    cumulative.plot.area(ax = axs)
-    axs.set_xlabel('Time')
-    axs.set_ylabel('Cumulative Income')
-    fig.autofmt_xdate()
-    fig.savefig('Cumulative Sales.png')
-    return fig
+    @staticmethod
+    def sales_over_time_plot(df):
+        sales_per_day = Analyser.sales_over_time(df)
+        fig, axs = plt.subplots()
+        sales_per_day.plot(ax = axs)
+        axs.set_xlabel('Date')
+        axs.set_ylabel('Income')
+        fig.autofmt_xdate()
+        fig.savefig('Sales over Time.png')
+        return fig
 
-def sales_of_topselling_product_plot(df, x = 10):
-    sales_per_product = top_x_price(df,x)
-    number_of_product = top_x_units(df,x)
-    fig, axs = plt.subplots()
-    pd.concat([sales_per_product, number_of_product], axis = 1).plot.bar(ax=axs)
-    fig.savefig(f'Sales by {x} best-selling Product.png')
-    return fig
+    @staticmethod
+    def cumulative_sales_plot(df):
+        cumulative = Analyser.cumulative_sales(df)
+        fig, axs = plt.subplots(figsize= (40,4))
+        cumulative.plot.area(ax = axs)
+        axs.set_xlabel('Time')
+        axs.set_ylabel('Cumulative Income')
+        fig.autofmt_xdate()
+        fig.savefig('Cumulative Sales.png')
+        return fig
 
-def sales_price_dist_plot(df):
-    fig,axs = plt.subplots()
-    df['Sale Price'].plot.box(ax = axs)
-    axs.set_ylabel('Price')
-    fig.savefig('Box-plot of price dist.png')
-    return fig
+    @staticmethod
+    def sales_of_topselling_product_plot(df, x = 10):
+        sales_per_product = Analyser.top_x_price(df,x)
+        number_of_product = Analyser.top_x_units(df,x)
+        fig, axs = plt.subplots()
+        pd.concat([sales_per_product, number_of_product], axis = 1).plot.bar(ax=axs)
+        fig.savefig(f'Sales by {x} best-selling Product.png')
+        return fig
 
-
-def price_dist_by_products_plot(df):
-    fig, axs = plt.subplots()
-    pivoted = pd.pivot(df,columns='Product', values = 'Sale Price')
-    pivoted.plot.box(ax = axs)
-    axs.set_ylabel('Price')
-    fig.savefig('Box-plots of price distribution by category.png')
-    return fig
-
-def gender_dist_plot(df):
-    fig, axs = plt.subplots()
-    df.groupby('Category', observed=True)['Units Sold'].sum().plot.pie(ax = axs)
-    fig.savefig('Pie-chart of transactions by gender.png')
-    return fig
+    @staticmethod
+    def sales_price_dist_plot(df):
+        fig,axs = plt.subplots()
+        df['Sale Price'].plot.box(ax = axs)
+        axs.set_ylabel('Price')
+        fig.savefig('Box-plot of price dist.png')
+        return fig
 
 
+    @staticmethod
+    def price_dist_by_products_plot(df):
+        fig, axs = plt.subplots()
+        pivoted = pd.pivot(df,columns='Product', values = 'Sale Price')
+        pivoted.plot.box(ax = axs)
+        axs.set_ylabel('Price')
+        fig.savefig('Box-plots of price distribution by category.png')
+        return fig
+
+    @staticmethod
+    def gender_dist_plot(df):
+        fig, axs = plt.subplots()
+        plt.setp(axs.get_xticklabels(), rotation=30, horizontalalignment='right')
+        data = df.groupby('Category', observed=True)['Units Sold'].sum()
+        data.plot.bar(ylim=(data.min()-(data.max()-data.min())/10, data.max()+(data.max()-data.min())/10))
+        fig.savefig('Bar-chart of transactions by gender.png')
+        return fig
 
