7fe70a1f253eb69befac87b74ec4c0c8fedd1eeb
jravn17 <47786660+jravn17@users.noreply.github.com>
2024-03-13 15:23:55 +0100

    Update datareader.py

--- SPAC Uge 6/datareader.py
+++ SPAC Uge 6/datareader.py
@@ -1,26 +1,29 @@
 import pandas as pd
 
-df_salesdata = pd.read_csv('tøjfirma_sales_data.csv', sep=',')
+df_salesdata = pd.read_csv('bedre_sales_data.csv', sep=',')
 
-# Tilføjer så vi sikre vi har indlæst kolonnerne korekt
-df_salesdata.columns = ['Product','Category','Size','Units Sold', 'Sale Price', 'Sale Date']
+class DataReader:
 
-# Fjerner duplikater i filen
-df_salesdata.drop_duplicates(inplace=True)
+    def load_data(df_salesdata):
+        # Tilføjer så vi sikre vi har indlæst kolonnerne korrekt
+        df_salesdata.columns = ['Product','Category','Size','Units Sold', 'Sale Price', 'Sale Date']
 
-# Fjern rækker med manglende eller ugyldige værdier
-df_salesdata.dropna(subset=['Product','Category','Size','Units Sold', 'Sale Price', 'Sale Date'], inplace=True)
+        # Fjerner duplikater i filen
+        df_salesdata.drop_duplicates(inplace=True)
 
-#Konvertering 'Product_Type' og 'Weather'  kolonner til kategorisk værdi
-df_salesdata['Product'] = df_salesdata['Product'].astype('category')
-df_salesdata['Category'] = df_salesdata['Category'].astype('category')
-df_salesdata['Size'] = df_salesdata['Size'].astype('category')
+        # Fjern rækker med manglende eller ugyldige værdier
+        df_salesdata.dropna(subset=['Product','Category','Size','Units Sold', 'Sale Price', 'Sale Date'], inplace=True)
 
-# Konvertering kolonner til numeriske værdier
-df_salesdata['Units Sold'] = df_salesdata['Units Sold'].astype(int)
-df_salesdata['Sale Price'] = df_salesdata['Sale Price'].astype(float)
+        # Konvertering kolonner til kategorisk værdi
+        df_salesdata['Product'] = df_salesdata['Product'].astype('category')
+        df_salesdata['Category'] = df_salesdata['Category'].astype('category')
+        df_salesdata['Size'] = df_salesdata['Size'].astype('category')
 
-#Konvertering 'Date' kolonne til datetime format
-df_salesdata['Sale Date'] = pd.to_datetime(df_salesdata['Sale Date'])
+        # Konvertering kolonner til numeriske værdier
+        df_salesdata['Units Sold'] = df_salesdata['Units Sold'].astype(int)
+        df_salesdata['Sale Price'] = df_salesdata['Sale Price'].astype(float)
 
-#print(df_salesdata)
+        # Konvertering 'Date' kolonne til datetime format
+        df_salesdata['Sale Date'] = pd.to_datetime(df_salesdata['Sale Date'])
+
+        return df_salesdata
