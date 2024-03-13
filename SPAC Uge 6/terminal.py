eca3db680ded5a3a1c56b0bdd9d903bb9bd200c9
MetMet7 <159765915+MetMet7@users.noreply.github.com>
2024-03-13 10:30:40 +0100

    Update terminal.py

--- SPAC Uge 6/terminal.py
+++ SPAC Uge 6/terminal.py
@@ -1,12 +1,8 @@
-from data_analyser import *
-from datareader import *
-from data_visualiser import *
+from data_analyser import Analyser
+from data_reader import df_salesdata
+from data_visualiser import Plots
 import matplotlib.pyplot as plt
 import os
-
-
-
-
 
 #we are not gonna do math on these numbers
 while True:
@@ -24,46 +20,46 @@ while True:
     choice = choice.strip()
 
     if choice == '1':
-        total_sales_amount, total_transactions = total_sales(df_salesdata)
+        total_sales_amount, total_transactions = Analyser.total_sales(df_salesdata)
         print("Total Salg:", total_sales_amount)
         print("Total Transaktioner:", total_transactions)
     elif choice == '2':
-        print("Gennemsnitlig salg:", avg_sales(df_salesdata))
+        print("Gennemsnitlig salg:", Analyser.avg_sales(df_salesdata))
     elif choice == '3':
         print("Top 5 Units Solgt:")
-        print(top_x_units(df_salesdata))
+        print(Analyser.top_x_units(df_salesdata))
     elif choice == '4':
         print("Top 5 Priser:")
-        print(top_x_price(df_salesdata))
+        print(Analyser.top_x_price(df_salesdata))
     elif choice == '5':
         print("Kønsfordeling:")
-        print(gender(df_salesdata))
+        print(Analyser.gender(df_salesdata))
 
     elif choice == '6':
         print("Salg Over Tid:")
-        print(sales_over_time_plot(df_salesdata))
+        print(Analyser.sales_over_time_plot(df_salesdata))
     elif choice == '7':
         print("Vælg plot")
-        print("1) Sales over Time")
-        print("2) Cumulative Sales")
-        print("3) Sales of Top Selling Product")
-        print("4) Sales Price Distribution")
-        print("5) Price Distribution by Products")
-        print("6) Gender Distribution")
+        print("1) Salg over tid")
+        print("2) Kumulative salg")
+        print("3) Salg per produkt")
+        print("4) Salgspris distribution")
+        print("5) Salgspris distribution per produkt")
+        print("6) Kønsfordeling")
         print("7) Quit")
         plot_choice = input("Enter your choice: ").strip()
         if plot_choice == '1':
-            sales_over_time_plot(df_salesdata)
+            Plots.sales_over_time_plot(df_salesdata)
         elif plot_choice == '2':
-            cumulative_sales_plot(df_salesdata)
+            Plots.cumulative_sales_plot(df_salesdata)
         elif plot_choice == '3':
-            sales_of_topselling_product_plot(df_salesdata)
+            Plots.sales_of_topselling_product_plot(df_salesdata)
         elif plot_choice == '4':
-            sales_price_dist_plot(df_salesdata)
+            Plots.sales_price_dist_plot(df_salesdata)
         elif plot_choice == '5':
-            price_dist_by_products_plot(df_salesdata)
+            Plots.price_dist_by_products_plot(df_salesdata)
         elif plot_choice == '6':
-            gender_dist_plot(df_salesdata)
+            Plots.gender_dist_plot(df_salesdata)
         elif plot_choice == '7':
             break
         else:
