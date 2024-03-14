a071c5e9a70f4e3b4c66a4650d997a5b8cc4604a
MetMet7 <159765915+MetMet7@users.noreply.github.com>
2024-03-14 11:45:40 +0100

    Update dataanalyser.py

--- SPAC Uge 6/dataanalyser.py
+++ SPAC Uge 6/dataanalyser.py
@@ -1,53 +1,54 @@
+
 class Analyser:
 
     @staticmethod
     def total_sales(df, filters = []):
         if not filters:
             try:
-                return df['Sale Price'].sum(), None
+                return df['Sale Price'].sum(), len(df)
             except ValueError as e:
                 return None, e
         else:
             try:
-                sales = df.groupby(filters)['Sale Price'].sum()
-                return sales, None
+                sales = df.groupby(filters, observed=True)['Sale Price'].sum()
+                return sales, len(sales)
             except (ValueError, KeyError) as e:
                 return None, e
-
+ 
     @staticmethod
     def avg_sales(df, filters =[]):
         if not filters:
             try:
-                return df['Sale Price'].mean(), None
+                return df['Sale Price'].mean(), len(df)
             except ValueError as e:
                 return None, e
         else:
             try:
-                sales = df.groupby(filters)['Sale Price'].mean()
-                return sales, None
+                sales = df.groupby(filters, observed=True)['Sale Price'].mean()
+                return sales, len(sales)
             except (ValueError, KeyError) as e:
                 return None, e
             
     @staticmethod
     def top_x_units(df, x=5):
-        return df.groupby('Product')['Units Sold'].sum().nlargest(x)
+        return df.groupby('Product', observed = True)['Units Sold'].sum().nlargest(x)
 
     @staticmethod
     def top_x_price(df, x=5):
-        return df.groupby('Product')['Sale Price'].sum().nlargest(x)
+        return df.groupby('Product', observed = True)['Sale Price'].sum().nlargest(x)
 
     @staticmethod
     def gender(df):
-        return df.groupby('Category')['Units Sold'].sum()
+        return df.groupby('Category', observed = True)['Units Sold'].sum()
     
     @staticmethod
     def sales_over_time(df,):
         return df.groupby([df['Sale Date'].dt.year.values,
-                            df['Sale Date'].dt.month.values])\
-                                ['Sale Price '].sum()
+                            df['Sale Date'].dt.month.values], observed = True)['Sale Price'].sum()
         
     @staticmethod
     def cumulative_sales(df):
         return Analyser.sales_over_time(df).cumsum() 
+     
     
 
